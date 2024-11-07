5ce92961a5950b1d308752de862cd7174bc26556
Aulia Putri Utami <aulia1700016113@webmail.uad.ac.id>
2024-11-07 16:45:01 +0700

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/positivitus-reactjs-ui/',
+    base: '/positivitus-react-ui/',
     css: {
         preprocessorOptions: {
             scss: {
